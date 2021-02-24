dd811ac5da15a4159432654db7800f6de41fd4a0
Christian Haider <ChristianHaider@users.noreply.github.com>
2021-02-24 15:07:55 +0100

    bump versions

--- deploy.js
+++ deploy.js
@@ -6,7 +6,7 @@ define([
     'css!amber-covid19view/css/tree',
     'css!amber-covid19view/css/about',
     'css!amber-covid19view/css/graphics',
-    'css!amber-covid19view/css/chart',
+    'css!amber-covid19view/css/chart'
     // --- packages to be deployed end here ---
 ], function (amber) {
     return amber;
